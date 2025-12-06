9d7fd629f60537a8aa9582357dbab4459b43ad72
Qquazld <yannletertre10@gmail.com>
2025-12-06 02:43:03 +0100

    feat: API + CRUD + FrontEnd connexion with Axios

--- server/controllers/artController.js
+++ server/controllers/artController.js
@@ -1,14 +1,100 @@
 import Art from "../models/Art.js";
 import mongoose from "mongoose";
 
-// @Description: Récupérer toutes les œuvres d'art
+// @Description: READ all artworks
 // @Route: GET /api/art
 // @Access: Public
-
 export const getAllArt = async (req, res) => {
   try {
     const artworks = await Art.find().sort({ createdAt: -1 });
     res.status(200).json(artworks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @Description: CREATE a new artwork
+// @Route: POST /api/art
+// @Access: Private
+export const createArt = async (req, res) => {
+  const { title, artist, description, imageUrl, year } = req.body;
+
+  if (!title || !artist || !imageUrl) {
+    return res
+      .status(400)
+      .json({ message: "Title, artist and imageUrl are required." });
+  }
+
+  try {
+    const newArt = new Art({
+      title,
+      artist,
+      description,
+      imageUrl,
+      year,
+    });
+    const savedArt = await newArt.save();
+    res.status(201).json(savedArt);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+// @desc    READ one artwork by ID
+// @route   GET /api/art/:id
+// @access  Public
+export const getArtbyId = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "Invalid ID." });
+  }
+  try {
+    const artwork = await Art.findById(id);
+    if (!artwork) {
+      return res.status(404).json({ message: "Artwork not found." });
+    }
+    res.status(200).json(artwork);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    DELETE one artwork by ID
+// @route   DELETE /api/art/:id
+// @access  Private
+export const deleteArtbyId = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "Invalid ID." });
+  }
+  try {
+    const deletedArt = await Art.findByIdAndDelete(id);
+    if (!deletedArt) {
+      return res.status(404).json({ message: "Artwork not found." });
+    }
+    res.status(200).json({ message: "Artwork deleted successfully." });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    UPDATE one artwork by ID
+// @route   PATCH /api/art/:id
+// @access  Private
+
+export const updateArtbyId = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ message: "Invalid ID." });
+
+  try {
+    const updatedArt = await Art.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedArt)
+      return res.status(404).json({ message: "Artwork not found." });
+    res.status(200).json(updatedArt);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- server/routes/artRoutes.js
+++ server/routes/artRoutes.js
@@ -1,10 +1,28 @@
 import express from "express";
-import { getAllArt } from "../controllers/artController";
+import {
+  createArt,
+  getAllArt,
+  getArtbyId,
+  deleteArtbyId,
+  updateArtbyId,
+} from "../controllers/artController.js";
 
 // Création d'une instance de Router Express
 const router = express.Router();
 
 // Route GET /api/art/
 router.get("/", getAllArt);
+
+// Route POST /api/art/
+router.post("/", createArt);
+
+// Route GET by ID
+router.get("/:id", getArtbyId);
+
+// Route DELETE by ID
+router.delete("/:id", deleteArtbyId);
+
+// Route UPDATE by ID
+router.patch("/:id", updateArtbyId);
 
 export default router;
